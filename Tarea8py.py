c830302fbbaa536e20eab26ae82b7ab71b31c6d3
lFranco3 <jcefala95@gmail.com>
2025-08-14 11:01:43 -0400

    Add files via upload

---
+++ Tarea8py.py
@@ -0,0 +1,13 @@
+#jean
+nota = float(input("ingrese su nota del 1 al 100: "))
+
+if nota >= 80 and nota <= 100:
+    print("Tu nota es A")
+if nota >= 60 and nota <= 79:
+    print("Tu nota es B")
+if nota >= 40 and nota <= 59:
+    print("Tu nota es c")
+if nota >= 20 and nota <= 39:
+    print("Tu nota es D")
+if nota >= 0 and nota <= 19:
+    print("Tu nota es F")
