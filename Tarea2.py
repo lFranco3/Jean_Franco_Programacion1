58d36961f5981892d40c92019c543b4c602d7160
lFranco3 <jcefala95@gmail.com>
2025-08-19 00:20:27 -0400

    Delete Tarea2.py

--- Tarea2.py
+++
@@ -1,5 +0,0 @@
-#Jean Franco
-Nombre = input("ingresa tu nombre: ")
-edad = int(input("\ningresa tu edad: "))
-ciudad = input("\ningresa la ciudad en la que vives: ")
-print(f"\nTu nombre es {Nombre}, tienes {edad} años de edad y vives en {ciudad}")
