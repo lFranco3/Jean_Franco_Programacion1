786bebf015fc2e627aa26c667bb41b1bcc52d297
lFranco3 <jcefala95@gmail.com>
2025-08-19 00:21:20 -0400

    Delete Tarea6.py

--- Tarea6.py
+++
@@ -1,9 +0,0 @@
-#Jean Franco
-print("clasificador de numeros de positivos o negativos")
-numero = float(input("Ingrese el numero a verificar: "))
-if numero > 1:
-    print("el numero ingresado es positivo")
-elif numero == 0:
-    print("el numero ingresado es cero")
-else:
-    print("el numero ingresado es negativo")
