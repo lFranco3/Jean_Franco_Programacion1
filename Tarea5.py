e31a7f07d4a2c81ff9071b1e8634c1a991e29ee3
lFranco3 <jcefala95@gmail.com>
2025-08-19 00:20:35 -0400

    Delete Tarea5.py

--- Tarea5.py
+++
@@ -1,7 +0,0 @@
-#Jean Franco
-print("calculadora de area y perimetro de un rectangulo")
-base = float(input("introduza los valores de la base: "))
-altura = float(input("introduzca los varoles de la altura: "))
-area = base * altura
-perimetro = 2 * base + 2 * altura
-print(f"El area del rectangulo es {area} y su perimetro es {perimetro}")
