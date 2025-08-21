0f11b9cb1bd8d49caffd629e3b92542256de609d
lFranco3 <jcefala95@gmail.com>
2025-08-21 14:21:30 -0400

    Add files via upload

---
+++ HTML y CSS/DOM.js
@@ -0,0 +1,10 @@
+
+document.getElementById('elpepe'); {
+    document.getElementById('elpepe').style.color = 'red';
+}   
+
+document.getElementById('boton'); {
+    document.getElementById('boton').style.backgroundColor = 'red'
+}
+
+
