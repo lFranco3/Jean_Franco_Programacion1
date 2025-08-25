5196329ea3d76120623713c3ce1c1d1c9b47fa92
lFranco3 <jcefala95@gmail.com>
2025-08-25 16:57:01 -0400

    Add files via upload

--- PHP/calculadora.php
+++ PHP/calculadora.php
@@ -1,8 +1,5 @@
 <?php
 
-$a = readline("Ingrese primero numero: ");
-$b = readline("Ingrese segundo numero: ");
-
 function suma($a, $b) {
     $suma = $a + $b;
     echo "La suma de $a y $b es: $suma\n";
@@ -20,9 +17,42 @@ function division($a, $b) {
     echo "La resta de $a y $b es: $division\n";
 }
 
-suma($a,$b);
-resta($a,$b);
-multiplicacion($a,$b);
-division($a,$b);
+do {
+
+echo"===== MENU ===== \n";
+echo" 1. suma \n 2. resta \n 3. multiplicacion \n 4. division \n 5. Salir \n";
+$opcion = "Ingrese la operacion a realizar";
+
+if ($opcion == 1) {
+    $a = readline("Ingrese primero numero a sumar: ");
+    $b = readline("Ingrese segundo numero a sumar: ");
+    suma($a, $b);
+}
+
+else if ($opcion == 2) {
+    $a = readline("Ingrese primero numero a restar: ");
+    $b = readline("Ingrese segundo numero a restar: ");
+    resta($a ,$b);
+}
+
+else if ($opcion == 3) {
+    $a = readline("Ingrese primero numero a multiplicar: ");
+    $b = readline("Ingrese segundo numero a multiplicar: ");
+    multiplicacion($a,$b);
+}
+
+else if ($opcion == 4) {
+    $a = readline("Ingrese primero numero a dividir: ");
+    $b = readline("Ingrese segundo numero a dividir: ");
+    division($a,$b);
+}
+
+else {
+    echo "Error: Ingrese una opcion valida";
+}
+
+} while ($opcion == 5);
+
+echo "saliendo...";
 
 ?>
