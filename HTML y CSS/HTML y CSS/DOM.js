17dd5d8091ee2336e65dbffdd993e934dc0f3ff3
lFranco3 <jcefala95@gmail.com>
2025-08-26 16:57:14 -0400

    Add files via upload

---
+++ HTML y CSS/HTML y CSS/DOM.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const runSelectionBtn = document.getElementById('boton');
+
+    runSelectionBtn.addEventListener('click', () => {
+
+    const fondo = document.getElementById('fondo');
+    fondo.style.background = '#0c0c0c';
+    const letra = document.getElementById('elpepe')
+    letra.style.color = 'red'
+
+});
+})
